8e000c7e304e584a8811b7510d2e139fa82143ce
defaultname <westn666@gmail.com>
2022-01-31 17:21:37 +0300

    test controller layer (in progress)

--- src/test/java/com/store/springbootstoreex/controller/CategoryControllerTest.java
+++ src/test/java/com/store/springbootstoreex/controller/CategoryControllerTest.java
@@ -1,5 +1,6 @@
 package com.store.springbootstoreex.controller;
 
+import com.store.springbootstoreex.domain.Category;
 import com.store.springbootstoreex.service.CategoryService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,14 +10,14 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = CategoryController.class)
 @MockBean(UserDetailsService.class)
+@WithMockUser(authorities = "ADMIN") // Контроллер доступен только для админов, поэтому мокаем юзера с правами admin
 class CategoryControllerTest {
 
     @Autowired
@@ -25,30 +26,64 @@ class CategoryControllerTest {
     @MockBean
     private CategoryService categoryService;
 
+    private final Long id = 1L;
+
     @Test
-    @WithMockUser(authorities = "ADMIN")
     void createCategory() throws Exception {
+        Category category = new Category("smart");
+        // Тест GET-запроса
         this.mockMvc.perform(get("/category/new"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("createCategory"));
+                .andExpectAll(
+                        status().isOk(),
+                        view().name("createCategory")
+                );
+        // Тесты POST-запросов
+        this.mockMvc.perform(post("/category/new")
+                        .flashAttr("category", category))
+                .andExpect(view().name("redirect:/admin"));
 
-//        this.mockMvc.perform(post("/category/new"))
-//                .
+        verify(categoryService, times(1)).saveCategory(category);
+
+        category.setCategoryName(null); // Ставим наименование категории на некорректное (т.е. на null)
+        this.mockMvc.perform(post("/category/new")
+                        .flashAttr("category", category))
+                .andExpect(model().hasErrors()); // И получаем ошибку в Binding Result
     }
 
     @Test
-    void testCreateCategory() {
+    void editCategory() throws Exception {
+        Category category = new Category("smart");
+        when(categoryService.getCategoryById(id)).thenReturn(category);
+        // Тест GET-запроса
+        this.mockMvc.perform(get("/category/edit/1"))
+                .andExpectAll(
+                        status().isOk(),
+                        model().attribute("categoryForm", category),
+                        view().name("editCategory")
+                );
+
+        verify(categoryService, times(1)).getCategoryById(id);
+        // Тесты POST-запросов
+        this.mockMvc.perform(post("/category/edit/1")
+                        .flashAttr("category", category))
+                .andExpect(view().name("redirect:/admin"));
+
+        verify(categoryService, times(1)).saveCategory(category);
+
+        category.setCategoryName(null); // Ставим наименование категории на некорректное (т.е. на null)
+        this.mockMvc.perform(post("/category/edit/1")
+                        .flashAttr("category", category))
+                .andExpect(model().hasErrors()); // И получаем ошибку в Binding Result
     }
 
     @Test
-    void editCategory() {
-    }
+    void deleteCategory() throws Exception {
+        this.mockMvc.perform(post("/category/delete/1"))
+                .andExpectAll(
+                        status().is3xxRedirection(),
+                        view().name("redirect:/admin")
+                );
 
-    @Test
-    void testEditCategory() {
-    }
-
-    @Test
-    void deleteCategory() {
+        verify(categoryService, times(1)).deleteById(id);
     }
 }
---
+++ src/test/java/com/store/springbootstoreex/controller/ReviewControllerTest.java
@@ -0,0 +1,59 @@
+package com.store.springbootstoreex.controller;
+
+import com.store.springbootstoreex.domain.Product;
+import com.store.springbootstoreex.domain.Review;
+import com.store.springbootstoreex.domain.User;
+import com.store.springbootstoreex.service.ReviewService;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(controllers = ReviewController.class)
+@MockBean(UserDetailsService.class)
+@WithMockUser(authorities = "USER")
+class ReviewControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ReviewService reviewService;
+
+    @Test
+    void addComment() throws Exception {
+        Review review = new Review("test", new User(), new Product(), 4);
+        this.mockMvc.perform(post("/comments/new")
+                        .flashAttr("review", review))
+                .andExpect(view().name("redirect:/index"));
+
+        verify(reviewService, times(1)).saveReview(review);
+
+        review.setRating(-1); // Делаем отзыв некорректным (отрицательная оценка)
+        this.mockMvc.perform(post("/comments/new")
+                        .flashAttr("review", review))
+                .andExpect(model().hasErrors()); // И получаем ошибку в Binding Result
+    }
+
+    @Test
+    @WithMockUser(authorities = "ADMIN")
+    void deleteComment() throws Exception {
+        this.mockMvc.perform(post("/comments/delete/1"))
+                .andExpectAll(
+                        status().is3xxRedirection(),
+                        view().name("redirect:/index")
+                );
+
+        Long id = 1L;
+        verify(reviewService, times(1)).deleteReviewById(id);
+    }
+}
